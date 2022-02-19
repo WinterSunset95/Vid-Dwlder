49f381132e416d18eeef3a685fd17038e31b13a0
Winter <90015510+WinterSunset95@users.noreply.github.com>
2022-02-19 04:11:59 -0500

    update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,13 @@
 
+const inquirer = require('inquirer');
+
+const question = [
+    {
+        type: 'input',
+        name: 'url',
+        message: 'Link to video: ',
+    }
+]
+
+const question = inquirer.prompt(question);
+console.log(question);
