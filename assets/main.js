9e0ccd3c4cdc9441637b47f570c4321984750504
WinterSunset95 <shirookamicod@gmail.com>
2022-02-19 22:09:32 +0530

    first commit on the development branch

--- assets/main.js
+++ assets/main.js
@@ -13,6 +13,6 @@ const inquirer = require('inquirer');
     const { url } = await inquirer.prompt(question);
     console.log(url);
     const links = await link.downloadMedia('https://www.instagram.com/p/CL5QQPOJFl_/', "./");
-    console.log(links);
+    console.log();
 }
 )();
