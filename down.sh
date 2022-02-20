170af136612031ee136bae36ac9ada3e7bf9a4e1
WinterSunset95 <shirookamicod@gmail.com>
2022-02-20 12:58:47 +0530

    v1.1

--- down.sh
+++ down.sh
@@ -6,22 +6,21 @@ end="\e[m";
 
 devType=`cat .devType`
 
-clear
-printf "${red}
+logo () {
+    clear
+    printf "${red}
 
 
-██╗░░░██╗██╗██████╗░░░░░░░██████╗░░██╗░░░░░░░██╗██╗░░░░░██████╗░███████╗██████╗░
-██║░░░██║██║██╔══██╗░░░░░░██╔══██╗░██║░░██╗░░██║██║░░░░░██╔══██╗██╔════╝██╔══██╗
-╚██╗░██╔╝██║██║░░██║█████╗██║░░██║░╚██╗████╗██╔╝██║░░░░░██║░░██║█████╗░░██████╔╝
-░╚████╔╝░██║██║░░██║╚════╝██║░░██║░░████╔═████║░██║░░░░░██║░░██║██╔══╝░░██╔══██╗
-░░╚██╔╝░░██║██████╔╝░░░░░░██████╔╝░░╚██╔╝░╚██╔╝░███████╗██████╔╝███████╗██║░░██║
-░░░╚═╝░░░╚═╝╚═════╝░░░░░░░╚═════╝░░░░╚═╝░░░╚═╝░░╚══════╝╚═════╝░╚══════╝╚═╝░░╚═╝
+▒█░░▒█ ░▀░ █▀▀▄ ░░ ▒█▀▀▄ █░░░█ █░░ █▀▀▄ █▀▀ █▀▀█ 
+░▒█▒█░ ▀█▀ █░░█ ▀▀ ▒█░▒█ █▄█▄█ █░░ █░░█ █▀▀ █▄▄▀ 
+░░▀▄▀░ ▀▀▀ ▀▀▀░ ░░ ▒█▄▄▀ ░▀░▀░ ▀▀▀ ▀▀▀░ ▀▀▀ ▀░▀▀
 
-${other}
+    ${other}
 GitHub: WinterSunset95
 Instagram: winter_sunset_95
 
-${end}"
+    ${end}"
+}
 
 termux () {
     mv "$1.mp4" /sdcard/Downloaded_files
@@ -75,26 +74,76 @@ ig () {
     fi 
     main
 }
-
-function () {
-    printf '\n\nThis is a printed line'
-}
-main () {
-    printf "\n\nDownload from: \n1. YouTube\n2. Facebook\n3. Instagram\n4. Cancel\n"
-    read -n 2 -p "--> " input
-    printf "\n"
-    if [ "$input" = "1" ]
+song () {
+    printf "\nMp3 download"
+    printf "\nLink to target: "
+    read link
+    printf "Name the downloaded song: "
+    read name
+    youtube-dl --output "${name}.mp3" -f 140/m4a/bestaudio ${link}
+    if [ "$devType" = "termux" ]
     then
-        yt
+        termux "$name"
+    else 
+        linux "$name"
+    fi
+    main
+}
+default () {
+    printf "\nDefault downloader"
+    printf "\nLink to video: "
+    read link
+    printf "Name the downloaded video: "
+    read name
+    youtube-dl --output "${name}.mp4" -f best ${link}
+    if [ "$devType" = "termux" ]
+    then
+        termux "$name"
+    else
+        linux "$name"
+    fi 
+    main
+}
+
+main () {
+    logo
+    printf "
+
+Download from: 
+    1. Any site (default)
+    2. YouTube
+    3. Facebook
+    4. Instagram
+    5. Song Download
+    6. Cancel
+    "
+    read -p '--> ' input
+    printf "\n"
+    if [ "$input" = "" ]
+    then 
+        default
+    elif [ "$input" = "1" ]
+    then
+        default
     elif [ "$input" = "2" ] 
     then
-        fb
+        yt
     elif [ "$input" = "3" ]
     then
-        ig
+        fb
     elif [ "$input" = "4" ]
+    then 
+        ig 
+    elif [ "$input" = "5" ]
     then
-        printf "\nExiting\n"
+        song
+    elif [ "$input" = "6" ]
+    then
+        printf '
+Exit ---
+
+'
+ 
     else 
         printf "\nInvalid . . .\nPress any key to continue . .\n"
         read -n 1
